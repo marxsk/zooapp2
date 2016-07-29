853fab8b8a177fc6d0aae359e8051a7ca4dfe989
Marek 'marx' Grác <mgrac@redhat.com>
2016-07-29 16:44:27 +0200

    [feat] Component 'AnimalView' = basic view on text about animals

--- AnimalSwipe.ios.js
+++ AnimalSwipe.ios.js
@@ -7,7 +7,8 @@ import {
   ScrollView,
 } from 'react-native';
 
-var Swiper = require('react-native-swiper')
+var Swiper = require('react-native-swiper');
+var AnimalView = require('./AnimalView.ios');
 
 var styles = StyleSheet.create({
   wrapper: {
@@ -45,43 +46,33 @@ var swiper = React.createClass({
     return (
       <Swiper style={styles.wrapper} showsButtons={true} loop={false}>
         <View style={styles.slide1}>
-          <View style={{ flex: 0, flexDirection: 'row', alignItems: 'stretch',}}>
-            <Image source={require('./img/animal/hippo.png')} resizeMode='contain' style={{ height: 100, width: 300, }} />
-            <View style={{ flex: 1, }} />
-          </View>
-          <View style={{ paddingLeft: 30, paddingRight: 30, paddingTop: 10, }}>
-            <Text style={styles.text}>
-              Ahoj děti!  Jsem bazilišek a chtěl bych vám povědět něco o svých kamarádech, se kterými tady v zoo bydlím. Jsou to agamy kočinčinské, jeden sameček agamák a dvě samičky. (Samečka poznáte podle toho, že je barevnější, má na hřbetě větší hřeben a má žlutooranžově zbarvený spodek krku. Jinak jsou všechny agamy svrchu zelené, trochu do hněda a mají světlá bříška.)
-            </Text>
-          </View>
+          <AnimalView image='hippo-1'>
+            <Text style={{color: 'red'}}>Ahoj děti!</Text>  Jsem bazilišek a chtěl bych vám povědět něco o svých kamarádech, se kterými tady v zoo bydlím. Jsou to agamy kočinčinské, jeden sameček agamák a dvě samičky. (Samečka poznáte podle toho, že je barevnější, má na hřbetě větší hřeben a má žlutooranžově zbarvený spodek krku. Jinak jsou všechny agamy svrchu zelené, trochu do hněda a mají světlá bříška.)
+          </AnimalView>
         </View>
 
         <View style={styles.slide2}>
-          <View style={{ flex: 0, flexDirection: 'row', alignItems: 'stretch',}}>
-            <Image source={require('./img/animal/hippo.png')} resizeMode='contain' style={{ height: 100, width: 300, }} />
-            <View style={{ flex: 1, }} />
-          </View>
-          <View style={{ paddingLeft: 30, paddingRight: 30, paddingTop: 10, }}>
-            <Text style={styles.text}>
-              Agamy měří zhruba tři čtvrtě metru – takže jsou o něco delší než dvě dlouhá pravítka. Možná si říkáte, že až tak dlouhé nevypadají. To proto, že mají dlouhatánský ocas. Hodí se jim, když lezou po stromech nebo když plavou. Cože? Že jste nevěděly, že agamy umí plavat? No jéje! Agamy jsou ve vodě jako doma, a ještě se umí skvěle potápět. A aby toho nebylo málo, umí agamy taky utíkat jen po zadních nohou. A jak rychle!
-            </Text>
-          </View>
+          <AnimalView image='hippo-1'>
+            Agamy měří zhruba tři čtvrtě metru – takže jsou o něco delší než dvě dlouhá pravítka. Možná si říkáte, že až tak dlouhé nevypadají. To proto, že mají dlouhatánský ocas. Hodí se jim, když lezou po stromech nebo když plavou. Cože? Že jste nevěděly, že agamy umí plavat? No jéje! Agamy jsou ve vodě jako doma, a ještě se umí skvěle potápět. A aby toho nebylo málo, umí agamy taky utíkat jen po zadních nohou. A jak rychle!
+          </AnimalView>
         </View>
 
-        <View style={styles.slide3}>
-        <Text style={styles.text}>
-        Pokud si teď říkáte, že vás na agamách už nic nepřekvapí, jste na omylu: agamy mají tři oči! Dvě normální, těch jste si určitě všimly, a mezi nimi mají takový puntík, kterým vnímají jen světlo. A to tak dobře, že se dokážou probudit, když nad nimi letí dravec, který by je mohl chtít sníst. Agamy samotné se krmí hlavně hmyzem. Občas si pošmáknou i na nějaké rybce nebo myšce. Zato zelenina jim moc nevoní.
-        </Text>
-        </View>
         <View style={styles.slide1}>
-        <Text style={styles.text}>
-        Jak jsem říkal na začátku, tyhle agamy jsou z Kočinčíny, to je v Asii – na spodku Číny, kousku Vietnamu a tam kolem. Agamy totiž mají rády teplíčko; potřebují ho, aby mohly naklást vajíčka, která zahrabávají do země. A když je dost teplo, vylíhnou se z vajíček za nějaké dva měsíce malé agamky.
-        </Text>
+          <AnimalView image='hippo-1'>
+            Pokud si teď říkáte, že vás na agamách už nic nepřekvapí, jste na omylu: agamy mají tři oči! Dvě normální, těch jste si určitě všimly, a mezi nimi mají takový puntík, kterým vnímají jen světlo. A to tak dobře, že se dokážou probudit, když nad nimi letí dravec, který by je mohl chtít sníst. Agamy samotné se krmí hlavně hmyzem. Občas si pošmáknou i na nějaké rybce nebo myšce. Zato zelenina jim moc nevoní.
+          </AnimalView>
         </View>
+
+        <View style={styles.slide1}>
+          <AnimalView image='hippo-1'>
+            Jak jsem říkal na začátku, tyhle agamy jsou z Kočinčíny, to je v Asii – na spodku Číny, kousku Vietnamu a tam kolem. Agamy totiž mají rády teplíčko; potřebují ho, aby mohly naklást vajíčka, která zahrabávají do země. A když je dost teplo, vylíhnou se z vajíček za nějaké dva měsíce malé agamky.
+          </AnimalView>
+        </View>
+
         <View style={styles.slide2}>
-        <Text style={styles.text}>
-        No a na konec, připomínají vám agamy trochu jedno zlobivé pohádkové zvíře? Nejste samy, Angličané říkají agamám čínský vodní drak.
-        </Text>
+          <AnimalView image='hippo-1'>
+            No a na konec, připomínají vám agamy trochu jedno zlobivé pohádkové zvíře? Nejste samy, Angličané říkají agamám čínský vodní drak.
+          </AnimalView>
         </View>
       </Swiper>
     )
---
+++ AnimalView.ios.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+} from 'react-native';
+
+
+class AnimalView extends Component {
+  render() {
+    var pictures = {};
+    pictures["hippo-1"] = require('./img/animal/hippo.png');
+
+    let selectedImage = pictures[this.props.image];
+    return (
+      <View>
+        <Image source={selectedImage} resizeMode='contain' style={styles.headerImage} />
+        <View style={{ flex: 1, }} />
+        <View style={styles.textView}>
+          <Text style={styles.text}>
+            {this.props.children}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    height: 100,
+    width: 300,
+  },
+  textView: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 10,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 22,
+  }
+});
+
+module.exports = AnimalView;
