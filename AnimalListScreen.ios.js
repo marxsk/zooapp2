5a49f09cde1f8dedc189e921221b1e9b70f54706
Marek 'marx' Grác <mgrac@redhat.com>
2016-08-02 14:02:00 +0200

    [feat] Filtering

--- AnimalListScreen.ios.js
+++ AnimalListScreen.ios.js
@@ -4,6 +4,7 @@ import {
   View,
   Text,
   TouchableHighlight,
+  TextInput,
 } from 'react-native';
 
 var AlphabetListView = require('react-native-alphabetlistview');
@@ -81,7 +82,7 @@ var AnimalListScreen = React.createClass({
   },
   prepareSortedStructure(animals) {
     let state = {
-      data: {}
+      fullData: {}
     };
 
     for (let animal of animals) {
@@ -91,24 +92,53 @@ var AnimalListScreen = React.createClass({
         firstLetter = 'Ch';
       }
 
-      if (!(firstLetter in state.data)) {
-        state.data[firstLetter] = [];
+      if (!(firstLetter in state.fullData)) {
+        state.fullData[firstLetter] = [];
       }
 
-      state.data[firstLetter].push(animal);
+      state.fullData[firstLetter].push(animal);
     };
 
-    for (let letter in state.data) {
-      state.data[letter].sort(function(a, b) {
+    for (let letter in state.fullData) {
+      state.fullData[letter].sort(function(a, b) {
         return a.name.localeCompare(b.name);
       })
     };
 
+    state['data'] = state.fullData;
+
     return state;
+  },
+  setFilter(text) {
+    this.setState({
+      text: text.text,
+      data: this.filter(text.text.toUpperCase()),
+    })
+  },
+  filter(text) {
+    let data = {};
+    for (let letter in this.state.fullData) {
+      for (let idx in this.state.fullData[letter]) {
+        if (this.state.fullData[letter][idx].name.toUpperCase().includes(text)) {
+          if (!(letter in data)) {
+            data[letter] = [];
+          }
+          data[letter].push(this.state.fullData[letter][idx]);
+        }
+      }
+    }
+    return data;
   },
   render() {
     return (
       <View style={styles.container}>
+      <TextInput
+        style={{height: 40, textAlign: 'center', borderColor: 'gray', borderWidth: 1, backgroundColor: 'white'}}
+        onChangeText={(text) => this.setFilter({text})}
+        value={this.state.text}
+        placeholder='Search for Animal'
+        autoCorrect={false}
+      />
         <AlphabetListView
           data={this.state.data}
           cell={Cell}
